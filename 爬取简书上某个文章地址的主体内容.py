8386e657918e8ac34e3d649af2cce98417f23d4a
zmb4973992 <19725912@qq.com>
2017-03-28 09:55:51 +0800

    1
    1

---
+++ 爬取简书上某个文章地址的主体内容.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import requests
+import codecs
+
+def get_page(url):
+    headers = {
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+    }
+    data = requests.get(url, headers = headers).content
+    return data
+
+def get_text(html):
+    parser = BeautifulSoup(html, 'html.parser')
+    article = parser.find('div', attrs = {'class':'article'})
+    #定位文章
+    title = article.find('h1', attrs = {'class': 'title'}).get_text()
+    #获取标题
+    text = []
+    #创建空列表来存放文章
+    for paragraph in article.find_all('p'):
+        paragraph_content = paragraph.get_text()
+        text.append(paragraph_content)
+        #将文章一段一段的添加到列表中
+    return title, text
+
+def save_text(title, text):
+    file_name = title + '.txt'
+    with codecs.open(file_name,'wb', encoding = 'utf-8') as open_file:
+        try:
+            for p in text:
+                open_file.write('\t%s\r\n'%p)
+                #\t是tab制表符，\r\n是Carrige Return换行
+        except Exception:
+            print('发生了错误！')
+        print('文章抓取完成！')
+        return
+
+if __name__ == '__main__':
+    url = 'http://www.jianshu.com/p/293c3b71416e'
+    html = get_page(url)
+    title, text = get_text(html)
+    save_text(title, text)
